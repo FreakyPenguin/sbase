FreakyPenguin/sbase

commit 30fd43d7f3b8716054eb9867c835aadc423f652c

libutil/unescape.c: simplify and add \E
Signed-off-by: Mattias Andrée <maandree@kth.se>

libutil/unescape.c
@@ -1,74 +1,57 @@
 /* See LICENSE file for copyright and license details. */
+#include <ctype.h>
 #include <string.h>
 
 #include "../util.h"
 
+#define is_odigit(c)  ('0' <= c && c <= '7')
+
 size_t
 unescape(char *s)
 {
-	size_t len, i, off, m, factor, q;
+	static const char escapes[256] = {
+		['"'] = '"',
+		['\''] = '\'',
+		['\\'] = '\\',
+		['a'] = '\a',
+		['b'] = '\b',
+		['E'] = 033,
+		['e'] = 033,
+		['f'] = '\f',
+		['n'] = '\n',
+		['r'] = '\r',
+		['t'] = '\t',
+		['v'] = '\v'
+	};
+	size_t m, q;
+	char *r, *w;
 
-	len = strlen(s);
-
-	for (i = 0; i < len; i++) {
-		if (s[i] != '\\')
+	for (r = w = s; *r;) {
+		if (*r != '\\') {
+			*w++ = *r++;
 			continue;
-		off = 0;
-
-		switch (s[i + 1]) {
-		case '\\': s[i] = '\\'; off++; break;
-		case '\'': s[i] = '\'', off++; break;
-		case '"':  s[i] =  '"', off++; break;
-		case 'a':  s[i] = '\a'; off++; break;
-		case 'b':  s[i] = '\b'; off++; break;
-		case 'e':  s[i] =  033; off++; break;
-		case 'f':  s[i] = '\f'; off++; break;
-		case 'n':  s[i] = '\n'; off++; break;
-		case 'r':  s[i] = '\r'; off++; break;
-		case 't':  s[i] = '\t'; off++; break;
-		case 'v':  s[i] = '\v'; off++; break;
-		case 'x':
-			/* "\xH[H]" hexadecimal escape */
-			for (m = i + 2; m < i + 1 + 3 && m < len; m++)
-				if ((s[m] < '0' && s[m] > '9') &&
-				    (s[m] < 'A' && s[m] > 'F') &&
-				    (s[m] < 'a' && s[m] > 'f'))
-					break;
-			if (m == i + 2)
-				eprintf("invalid escape sequence '\\%c'\n", s[i + 1]);
-			off += m - i - 1;
-			for (--m, q = 0, factor = 1; m > i + 1; m--) {
-				if (s[m] >= '0' && s[m] <= '9')
-					q += (s[m] - '0') * factor;
-				else if (s[m] >= 'A' && s[m] <= 'F')
-					q += ((s[m] - 'A') + 10) * factor;
-				else if (s[m] >= 'a' && s[m] <= 'f')
-					q += ((s[m] - 'a') + 10) * factor;
-				factor *= 16;
-			}
-			s[i] = q;
-			break;
-		case '\0':
-			eprintf("null escape sequence\n");
-		default:
-			/* "\O[OOO]" octal escape */
-			for (m = i + 1; m < i + 1 + 4 && m < len; m++)
-				if (s[m] < '0' || s[m] > '7')
-					break;
-			if (m == i + 1)
-				eprintf("invalid escape sequence '\\%c'\n", s[i + 1]);
-			off += m - i - 1;
-			for (--m, q = 0, factor = 1; m > i; m--) {
-				q += (s[m] - '0') * factor;
-				factor *= 8;
-			}
-			s[i] = (q > 255) ? 255 : q;
 		}
-
-		for (m = i + 1; m <= len - off; m++)
-			s[m] = s[m + off];
-		len -= off;
+		r++;
+		if (!*r) {
+			eprintf("null escape sequence\n");
+		} else if (escapes[(unsigned char)*r]) {
+			*w++ = escapes[(unsigned char)*r++];
+		} else if (is_odigit(*r)) {
+			for (q = 0, m = 4; m && is_odigit(*r); m--, r++)
+				q = q * 8 + (*r - '0');
+			*w++ = MIN(q, 255);
+		} else if (*r == 'x' && isxdigit(r[1])) {
+			r++;
+			for (q = 0, m = 2; m && isxdigit(*r); m--, r++)
+				if (isdigit(*r))
+					q = q * 16 + (*r - '0');
+				else
+					q = q * 16 + (tolower(*r) - 'a' + 10);
+			*w++ = q;
+		} else {
+			eprintf("invalid escape sequence '\\%c'\n", *r);
+		}
 	}
 
-	return len;
+	return w - s;
 }

commit 9e4a3c85163bf9192538191ee6adf51b00f4d48b

libutil/unescape.c: ensure \0 termination if short
30fd43d caused '\n' to turn into '<LF>n'. For me this broke tr '\n' ' '. The
underlying problem is that unescape is no longer writing a terminating \0 byte
if the unescaped string is shorter.

Signed-off-by: Antoine Kaufmann <antoinek@cs.washington.edu>

libutil/unescape.c
@@ -52,6 +52,7 @@ unescape(char *s)
 			eprintf("invalid escape sequence '\\%c'\n", *r);
 		}
 	}
+        *w = 0;
 
 	return w - s;
 }